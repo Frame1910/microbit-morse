ae9d3296f9c54422bd32b484cda0954366642a47
Darren Meiring <Frame1910@users.noreply.github.com>
2018-08-20 12:59:09 +0800

    testing

--- Mu/mainFile.py
+++ Mu/mainFile.py
@@ -77,7 +77,7 @@ def radioRecieve():
         if data != None:
             if data == "exit":
                 exit()
-            else 
+            else:
                 break
     receivedString = data
     for x in range(5):
@@ -86,13 +86,40 @@ def radioRecieve():
     display.scroll("Received")
     print(receivedString)
     print("Translating")
-    
-    # REQUIRES TESTING -------------
+
+    # REQUIRES TESTING --------------------------------------------------------------------------
+    receivedString = ".- .-"
     if "." or "-" in receivedString:
-        morseEnglish(receivedString)
-    else
-        englishMorse(receivedString)
-    
+        # Split each Morse chracter by the spaces between them
+        receivedMorseList = receivedString.split(sep=" ")
+
+        receivedMorseString = []
+        # Loop the comparison of individualised Morse characters with their value in English
+        i = 0
+        while i < len(receivedMorseList):
+            # Add each translated value to a new array
+            receivedMorseString.append(morseToEnglish.get(receivedMorseList[i]))
+            i += 1
+        # Join each arrat value into one string
+        translatedString = " ".join(receivedMorseString)
+        print("Message Received:")
+        print(translatedString)
+    else:
+        # List each character as an array of characters
+        receivedEnglishArray = list(receivedString)
+
+        receivedEnglishString = []
+        # Loop the comparison of individualised English characters with their value in Morse
+        i = 0
+        while i < len(receivedEnglishArray):
+            # Add each new Morse value to a new array of morse characters
+            receivedEnglishString.append(englishToMorse.get(receivedEnglishArray[i]))
+            i += 1
+        # Join each arrat value into one string
+        translatedString = " ".join(receivedEnglishString)
+        print("Message Received:")
+        print(translatedString)
+
     while True:
         display.scroll("Reply?")
         if button_a.is_pressed():
@@ -100,7 +127,7 @@ def radioRecieve():
         if button_b.is_pressed():
             display.scroll("Goodbye")
             exit()
-    
+
 
 # TRANSLATION MODULES ---------------------------------------------------------------------------------------
 def englishMorse():
@@ -136,7 +163,7 @@ def englishMorse():
         radioRecieve()
 
 
-def morseEnglish(receivedString):
+def morseEnglish():
     inputMorse = input("Enter Morse you want to translate to English: ")
     print(inputMorse + " in English is:")
     # Split each Morse chracter by the spaces between them
---
+++ messageInterpreter.py
@@ -0,0 +1,35 @@
+import 
+# REQUIRES TESTING --------------------------------------------------------------------------
+receivedString = ".- .-"
+def messageInterpreter():
+    if "." or "-" in receivedString:
+      # Split each Morse chracter by the spaces between them
+      receivedMorseList = receivedString.split(sep=" ")
+
+      receivedMorseString = []
+      # Loop the comparison of individualised Morse characters with their value in English
+      i = 0
+      while i < len(receivedMorseList):
+          # Add each translated value to a new array
+          receivedMorseString.append(morseToEnglish.get(receivedMorseList[i]))
+          i += 1
+      # Join each arrat value into one string
+      translatedString = " ".join(receivedMorseString)
+      print("Message Received:")
+      print(translatedString)
+    else:
+      # List each character as an array of characters
+      receivedEnglishArray = list(receivedString)
+
+      receivedEnglishString = []
+      # Loop the comparison of individualised English characters with their value in Morse
+      i = 0
+      while i < len(receivedEnglishArray):
+          # Add each new Morse value to a new array of morse characters
+          receivedEnglishString.append(englishToMorse.get(receivedEnglishArray[i]))
+          i += 1
+      # Join each arrat value into one string
+      translatedString = " ".join(receivedEnglishString)
+      print("Message Received:")
+      print(translatedString)
+messageInterpreter()
